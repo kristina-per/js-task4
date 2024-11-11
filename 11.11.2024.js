819d5b7cd5336d311e535b10d7a2084b43267bed
kristina-per <pheradzekristina5@gmail.com>
2024-11-11 16:16:41 +0400

    Add files via upload

---
+++ 11.11.2024.js
@@ -0,0 +1,123 @@
+1. 
+const add = (a, b) =>
+{console.log (a+b)
+
+}
+add (2, 4)
+
+2.
+const function1 =(a, b) =>{
+    return a - b
+
+}
+console.log (function1(8,3))
+
+
+3
+const number=(num) =>{
+    console.log (num%2 ===0)
+}
+number (9)
+
+4.  
+ function calculateProduct (callback) {
+    const number1= 1
+    const number2= 20
+    return callback (number1, number2);
+}
+function multiplied (a,b) {
+    return a *  b
+}
+console.log (calculateProduct(multiplied))
+
+
+5.
+const isPositive=(num1) => {
+    console.log (num1 > 0)
+ 
+}
+isPositive (-10)
+
+
+6.
+const functionN=(n) =>{
+    return n*2
+}
+console.log (functionN(7))
+
+
+7.
+function calculate(callback) {
+    const numb=9
+    return callback (numb)
+}
+function multipliedX2 (numb) {
+    return numb *2
+}
+console.log (calculate(multipliedX2))
+
+8. 
+const isDivisibleByThree=(num8) => {
+    console.log (num8% 3 ===0)
+}
+isDivisibleByThree (7)
+
+9. 
+function evencalculate(callback) {
+    const num9=9
+    return callback (num9)
+}
+function even(numb9) {
+    return numb9 % 2 ===0
+}
+console.log (evencalculate(even))
+
+10. 
+const cube = (num10) => {
+    console.log (num10 * num10 *num10)
+}
+cube (2)
+
+11. 
+function calculateProduct1 (callback) {
+    const number1= 80
+    const number2= 20
+    return callback (number1, number2);
+}
+function multiplied1(a,b) {
+    return a *  b
+}
+console.log (calculateProduct1(multiplied1))
+
+12. 
+const isGreaterThanZero=(num12) => {
+    console.log (num12>0)
+}
+isGreaterThanZero (0)
+
+
+13. 
+const function13=(n13) => {
+    return n13/2
+}
+console.log (function13(10))
+
+
+14.
+function summcalculate (callback) {
+    const number14= 8
+    const number15= 2
+    return callback (number15, number14);
+}
+function summ(a,b) {
+    return a +  b
+}
+console.log (summcalculate(summ))
+
+
+15.
+const arrowFunction=(num16) => {
+    console.log (num16*num16)
+
+}
+arrowFunction (10)
